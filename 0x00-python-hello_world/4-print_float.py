7266b1a2798841fe68ad3f908696c1da3b6e5052
Ehigiepaul01 <paulehigie95@gmaol.com>
2022-06-27 15:23:41 -0700

    task 4

--- 0x00-python-hello_world/4-print_float.py
+++ 0x00-python-hello_world/4-print_float.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 number = 3.14159
-Float_number = "{.2f}".Float(number)
-print(f"Float: {round(number, 2)}")
-# print("number")
-# print("format_number")
+print(f"Float: {number:.2f}")
