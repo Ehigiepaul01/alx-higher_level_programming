821dc1189936cd5b100489e80dae8cf6172a7d2d
Ehigiepaul01 <paulehigie5@gmail.com>
2022-10-05 02:52:22 +0000

    task done

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,14 @@
-t Rectangle = require('./4-rectangle');
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
 
+/**
+ * Represents a Rectangle with 4 equal sides.
+ */
 class Square extends Rectangle {
+  /**
+   * Creates a new Square with the given dimensions.
+   * @size - The value of the width and height.
+   */
   constructor (size) {
     super(size, size);
   }
